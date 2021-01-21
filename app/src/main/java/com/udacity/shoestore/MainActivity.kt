91e311de66ac081cfc81c92c47109d8618e1af8d
Jean-Peer Lorenz <peer.loz@gmx.net>
2021-01-21 20:28:10 +0100

    Set toolbar as support action bar in main activity, refactor findNavController

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -3,6 +3,7 @@ package com.udacity.shoestore
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
@@ -15,12 +16,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
 
-        // get navController from navHostFragment, see:
-        // - https://developer.android.com/guide/navigation/navigation-getting-started#navigate
-        // - https://stackoverflow.com/a/63625046
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        val navController = findNavController()
 
         val appBarConfiguration =
             AppBarConfiguration(
@@ -30,7 +26,18 @@ class MainActivity : AppCompatActivity() {
                     R.id.shoesListFragment
                 )
             )
-        findViewById<Toolbar>(R.id.toolbar)
-            .setupWithNavController(navController, appBarConfiguration)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+    }
+
+    private fun findNavController(): NavController {
+        // get navController from navHostFragment, see:
+        // - https://developer.android.com/guide/navigation/navigation-getting-started#navigate
+        // - https://stackoverflow.com/a/63625046
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        return navHostFragment.navController
     }
 }
