0658fda6990c5a101e8eb92dee378f9fe02a46bf
Jean-Peer Lorenz <peer.loz@gmx.net>
2021-01-21 20:43:21 +0100

    Add overflow menu and user info.
    Add overflow menu for
    - adding predefined shoe items
    - clearing list of shoes
    
    Also, an info message was implemented to advise users in case
    no data is yet stored (thus shoe list screen is empty).

--- app/src/main/java/com/udacity/shoestore/screens/ShoesListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/ShoesListFragment.kt
@@ -1,14 +1,13 @@
 package com.udacity.shoestore.screens
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoesListBinding
 import com.udacity.shoestore.databinding.ListitemShoeBinding
@@ -20,10 +19,6 @@ class ShoesListFragment : Fragment() {
 
     private val model: ShoesViewModel by activityViewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,10 +27,19 @@ class ShoesListFragment : Fragment() {
             inflater, R.layout.fragment_shoes_list, container, false
         )
 
-        model.shoes.observe(viewLifecycleOwner, Observer {
+        model.shoes.observe(viewLifecycleOwner, {
+            if (it.size > 0) {
+                binding.shoesListEmptyText.visibility = View.GONE
+            } else {
+                binding.shoesListEmptyText.visibility = View.VISIBLE
+            }
+
+            // in order to not add items multiple times, remove all items form the
+            // linear layout first (note: this is not the best way to implement a list view)
+            binding.shoesListLayout.removeAllViews()
             for (item in it) {
-                Timber.i("${item}")
-                var itemBinding: ListitemShoeBinding = DataBindingUtil.inflate(
+                Timber.i("$item")
+                val itemBinding: ListitemShoeBinding = DataBindingUtil.inflate(
                     inflater,
                     R.layout.listitem_shoe,
                     binding.shoesListLayout,
@@ -49,6 +53,33 @@ class ShoesListFragment : Fragment() {
             findNavController().navigate(R.id.action_shoesListFragment_to_shoeDetailFragment)
         }
 
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.shoes_list_overflow_menu, menu)
+        menu.findItem(R.id.loadPredefinedData).isEnabled = !model.predefinedDataIsLoaded
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.loadPredefinedData -> {
+                model.addPredefinedShoes()
+                item.isEnabled = !model.predefinedDataIsLoaded
+                return true
+            }
+            R.id.clearShoeList -> {
+                model.clearShoesList()
+                return true
+            }
+        }
+
+        return NavigationUI.onNavDestinationSelected(
+            item,
+            requireView().findNavController()
+        ) || super.onOptionsItemSelected(item)
     }
 }
